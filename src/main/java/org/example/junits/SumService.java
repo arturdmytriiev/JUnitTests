8e0721c9f285508fca7289680a41bd2d1549d6a7
arturdmytriiev <arturdmytriiev@gmail.com>
2024-12-23 17:39:19 +0100

    Add action modify

--- src/main/java/org/example/junits/SumService.java
+++ src/main/java/org/example/junits/SumService.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SumService {
     public int add(int a, int b) {
-        int sum = a + b;
-        return sum;
+        return a+b;
     }
 
     public int multiply(int a, int b) {
